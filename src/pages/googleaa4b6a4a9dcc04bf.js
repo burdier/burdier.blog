e15c458109cb7561458bd6b06cc84cdb43185ee5
Bursoft <miguelluis@outlook.com>
2019-10-11 14:40:00 -0400

    add ownship google

---
+++ src/pages/googleaa4b6a4a9dcc04bf.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import React from 'react'
+
+export default function googleaa4b6a4a9dcc04bf() {
+    return (
+        <main>
+        google-site-verification: googleaa4b6a4a9dcc04bf.html
+        </main>
+    )
+}
